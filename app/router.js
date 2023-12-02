ef7c240b123d0e5314ac8533ec5178f268fd38cc
TSKHayagreevaC <srikanth.thiru12@gmail.com>
2023-12-02 15:00:12 +0530

    feat: Added about, contact routes.

--- app/router.js
+++ app/router.js
@@ -9,5 +9,5 @@ export default class Router extends EmberRouter {
 Router.map(function () {
   this.route('scientists');
   this.route('about');
-  this.route('contact', {path: '/getting-in-touch'})
+  this.route('contact', { path: '/getting-in-touch' });
 });
